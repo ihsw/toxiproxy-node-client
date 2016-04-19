6e1c8a08a76cdb17279c141bc0be00749a980e4a
Adrian Parker <iamhiddensomewhere@gmail.com>
2016-04-19 14:36:13 -0400

    Start making actual API requests to toxiproxy.

--- tests/hello-world.ts
+++ tests/hello-world.ts
@@ -1,4 +1,17 @@
 /// <reference path="../typings/main.d.ts" />
 import * as test from "tape";
+import * as request from "request";
+import * as HttpStatus from "http-status";
 
-console.log(test);
+test("Toxiproxy", (t: test.Test) => {
+  request.get("http://localhost:8474/proxies", (err, res, body) => {
+    t.equal(err, null, "err was not null");
+    if (err) {
+      t.end();
+      return;
+    }
+
+    t.equal(res.statusCode, HttpStatus.OK, "response was not OK");
+    t.end();
+  });
+});
