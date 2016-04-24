1ae2f55b7ec23e3e32eaafa42c2975723a4cf8e8
Adrian Parker <iamhiddensomewhere@gmail.com>
2016-04-24 19:03:46 -0400

    Create proxy removal test.

--- tests/Proxy.ts
+++ tests/Proxy.ts
@@ -1,6 +1,7 @@
 /// <reference path="../typings/main.d.ts" />
 import * as test from "tape";
 import Proxy from "../src/Proxy";
+import { ICreateProxyBody } from "../src/Toxiproxy";
 import { setup } from "./Helper";
 
 test("Proxy", (t: test.Test) => {
@@ -26,5 +27,31 @@ test("Proxy", (t: test.Test) => {
           st.end();
         });
     });
+  });
+
+  t.test("Should remove a proxy", (st: test.Test) => {
+    const { toxiproxy } = setup();
+
+    const createBody = <ICreateProxyBody>{
+      enabled: true,
+      listen: "localhost:0",
+      name: "remove-test",
+      upstream: "localhost:6379"
+    };
+    toxiproxy.createProxy(createBody)
+      .then((proxy: Proxy) => {
+        st.equal(createBody.name, proxy.name, "Create-body and proxy have same name");
+
+        proxy.remove()
+          .then(() => st.end())
+          .catch((err) => {
+            st.fail(err);
+            st.end();
+          });
+      })
+      .catch((err) => {
+        st.fail(err);
+        st.end();
+      });
   });
 });
