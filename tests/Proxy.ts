d615ab5436c01d24c6cd91db28e514eeb24907fb
Adrian Parker <iamhiddensomewhere@gmail.com>
2016-04-24 18:44:32 -0400

    Fix indentation.

--- tests/Proxy.ts
+++ tests/Proxy.ts
@@ -4,27 +4,27 @@ import Proxy from "../src/Proxy";
 import { setup } from "./Helper";
 
 test("Proxy", (t: test.Test) => {
-t.test("Should update a proxy", (st: test.Test) => {
-  const { helper } = setup();
+  t.test("Should update a proxy", (st: test.Test) => {
+    const { helper } = setup();
 
-  const proxyName = "update-test";
-  helper.withProxy(st, proxyName, (proxy: Proxy) => {
-    if (!proxy) {
-      st.fail("No proxy returned");
-      st.end();
-      return;
-    }
+    const proxyName = "update-test";
+    helper.withProxy(st, proxyName, (proxy: Proxy) => {
+      if (!proxy) {
+        st.fail("No proxy returned");
+        st.end();
+        return;
+      }
 
-    proxy.enabled = false;
-    proxy.update()
-      .then((updatedProxy) => {
-        st.equal(proxy.enabled, updatedProxy.enabled, "Created and updated proxy have same name");
-        st.end();
-      })
-      .catch((err) => {
-        st.fail(err);
-        st.end();
-      });
+      proxy.enabled = false;
+      proxy.update()
+        .then((updatedProxy) => {
+          st.equal(proxy.enabled, updatedProxy.enabled, "Created and updated proxy have same name");
+          st.end();
+        })
+        .catch((err) => {
+          st.fail(err);
+          st.end();
+        });
+    });
   });
-});
 });
