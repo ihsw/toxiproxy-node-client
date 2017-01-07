0a97a7e10f3e8f423228c94dc2d482ce54c2e823
Adrian Parker <iamhiddensomewhere@gmail.com>
2017-01-06 22:51:03 -0500

    Remove additional explicit casting.

--- tests/ToxicTypes.ts
+++ tests/ToxicTypes.ts
@@ -6,7 +6,7 @@ test("Proxy Should add a timeout toxic", async (t) => {
   const { proxy } = await createProxy(t, "add-timeout-toxic-test");
 
   const attributes = <Timeout>{ timeout: 5 * 1000 };
-  const toxic = await createToxic<Timeout>(t, proxy, "timeout", attributes);
+  const toxic = await createToxic(t, proxy, "timeout", attributes);
   t.is(attributes.timeout, toxic.attributes.timeout);
 
   return proxy.remove();
