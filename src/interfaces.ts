99b3ed6e53d5ae2e6b5d9d1c0444899e345f24be
Adrian Parker <iamhiddensomewhere@gmail.com>
2017-01-09 12:00:01 -0500

    Clean up interface naming.

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,38 +3,50 @@ import {
   Direction as ToxicDirection
 } from "./Toxic";
 
-// request & responses for GET /proxies
-export interface IGetProxiesResponse {
-  [name: string]: IGetProxyResponse;
-}
-
-// request & responses for POST /proxies
-export interface ICreateProxyBody {
+// misc
+export interface IProxyBody {
   name: string;
   listen: string;
   upstream: string;
   enabled?: boolean;
 }
 
-export interface ICreateProxyResponse {
+export interface IProxyResponse {
   name: string;
   listen: string;
   upstream: string;
   enabled: boolean;
-  toxics: IGetToxicResponse<any>[];
+  toxics: IToxicResponse<any>[];
 }
+
+export interface IToxicBody<T> {
+  name: string;
+  stream: ToxicDirection;
+  type: ToxicType;
+  toxicity: number;
+  attributes: T;
+}
+
+export interface IToxicResponse<T> extends IToxicBody<T> { }
+
+// request & responses for GET /proxies
+export interface IGetProxiesResponse {
+  [name: string]: IGetProxyResponse;
+}
+
+// request & responses for POST /proxies
+export interface ICreateProxyBody extends IProxyBody { }
+export interface ICreateProxyResponse extends IProxyResponse { }
 
 // request & responses for POST /populate
-export interface IPopulateProxiesBody {
-  [key: number]: ICreateProxyBody;
-}
+export interface IPopulateProxiesBody extends Array<IProxyBody> { }
 
 export interface IPopulateProxiesResponse {
-  proxies: ICreateProxyResponse[];
+  proxies: IProxyResponse[];
 }
 
 // request & responses for GET /proxies/{proxy}
-export interface IGetProxyResponse extends ICreateProxyResponse { }
+export interface IGetProxyResponse extends IProxyResponse { }
 
 // request & responses for POST /proxies/{proxy}
 export interface IUpdateProxyBody {
@@ -43,32 +55,24 @@ export interface IUpdateProxyBody {
   upstream: string;
 }
 
-export interface IUpdateProxyResponse extends ICreateProxyResponse { }
+export interface IUpdateProxyResponse extends IProxyResponse { }
 
 // request & responses for DELETE /proxies/{proxy}
 // intentionally left blank
 
 // request & responses for GET /proxies/{proxy}/toxics
-export interface IGetToxicsResponse<T> extends Array<IGetToxicResponse<T>> { }
+export interface IGetToxicsResponse<T> extends Array<IToxicResponse<T>> { }
 
 // request & responses for POST /proxies/{proxy}/toxics
-export interface ICreateToxicBody<T> {
-  name: string;
-  stream: ToxicDirection;
-  type: ToxicType;
-  toxicity: number;
-  attributes: T;
-}
-
-export interface ICreateToxicResponse<T> extends ICreateToxicBody<T> { }
+export interface ICreateToxicBody<T> extends IToxicBody<T> { }
+export interface ICreateToxicResponse<T> extends IToxicResponse<T> { }
 
 // request & responses for GET /proxies/{proxy}/toxics/{toxic}
-export interface IGetToxicResponse<T> extends ICreateToxicBody<T> { }
+export interface IGetToxicResponse<T> extends IToxicResponse<T> { }
 
 // request & responses for POST /proxies/{proxy}/toxics/{toxic}
-export interface IUpdateToxicBody<T> extends ICreateToxicBody<T> { }
-
-export interface IUpdateToxicResponse<T> extends ICreateToxicBody<T> { }
+export interface IUpdateToxicBody<T> extends IToxicBody<T> { }
+export interface IUpdateToxicResponse<T> extends IToxicResponse<T> { }
 
 // request & responses for DELETE /proxies/{proxy}/toxics/{toxic}
 // intentionally left blank
