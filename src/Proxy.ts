90578aef50a9394ce20295e4fe8e943243b4e41b
Adrian Parker <iamhiddensomewhere@gmail.com>
2017-01-09 11:52:51 -0500

    Remove unused code.

--- src/Proxy.ts
+++ src/Proxy.ts
@@ -94,24 +94,6 @@ export default class Proxy {
     }
   }
 
-  // refreshToxics(): Promise<Proxy> {
-  //   return new Promise<Proxy>((resolve, reject) => {
-  //     request
-  //       .get(`${this.getPath()}/toxics`)
-  //       .end((err, res) => {
-  //         if (err) {
-  //           reject(err);
-  //           return;
-  //         } else if (res.status !== HttpStatus.OK) {
-  //           reject(new Error(`Response status was not ${HttpStatus.OK}: ${res.status}`));
-  //           return;
-  //         }
-
-  //         resolve(this);
-  //       });
-  //   });
-  // }
-
   async addToxic<T>(body: ICreateToxicBody<T>): Promise<Toxic<T>> {
     try {
       const toxic = await new Toxic(this, <ICreateToxicResponse<T>>await rp.post({
