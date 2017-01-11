0935f7c49689549379ef8620d5d9b056005e2c0b
Adrian Parker <iamhiddensomewhere@gmail.com>
2017-01-11 10:03:34 -0500

    Update index to re-export toxic types.
    Add typescript example file.

---
+++ example-code/typescript-example.ts
@@ -0,0 +1,30 @@
+import {
+    Toxiproxy,
+    ICreateProxyBody,
+    Toxic, ICreateToxicBody, Bandwidth
+} from "../src";
+
+const getToxic = async <T>(type: string, attributes: T): Promise<Toxic<T>> => {
+  const toxiproxy = new Toxiproxy("http://localhost:8474");
+  const proxyBody = <ICreateProxyBody>{
+    listen: "localhost:0",
+    name: "ihsw_test_redis_master",
+    upstream: "localhost:6379"
+  };
+  const proxy = await toxiproxy.createProxy(proxyBody);
+
+  const toxicBody = <ICreateToxicBody<T>>{
+      attributes: attributes,
+      type: type
+  };
+  return await proxy.addToxic(new Toxic(proxy, toxicBody));
+};
+
+// { attributes: { rate: 1000 },
+//   name: 'bandwidth_downstream',
+//   stream: 'downstream',
+//   toxicity: 1,
+//   type: 'bandwidth' }
+getToxic("bandwidth", <Bandwidth>{ rate: 1000 })
+  .then((toxic) => console.log(toxic.toJson()))
+  .catch(console.error);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-export { default as Proxy } from "./Proxy";
 export { ICreateProxyBody, ICreateToxicBody } from "./interfaces";
 export { default as Toxiproxy } from "./Toxiproxy";
+export { default as Proxy } from "./Proxy";
 export {
   default as Toxic,
   Direction as ToxicDirection,
   Type as ToxicType,
-  AttributeTypes as ToxicAttributeTypes
+  AttributeTypes as ToxicAttributeTypes,
+  Latency, Down, Bandwidth, Slowclose, Timeout, Slicer
 } from "./Toxic";
