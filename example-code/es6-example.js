5adff63b4a341636beaaeb04b93c0a606601499e
Harshit Sinha <harshitsinha1102@gmail.com>
2022-06-30 19:47:49 +0530

    Refactoring ES6 examples (#3)

--- example-code/es6-example.js
+++ example-code/es6-example.js
@@ -2,24 +2,20 @@
 const toxiproxyClient = require("../dist/");
 
 const getToxic = (type, attributes) => {
-  return new Promise((resolve, reject) => {
-    const toxiproxy = new toxiproxyClient.Toxiproxy("http://localhost:8474");
-    const proxyBody = {
-      listen: "localhost:0",
-      name: "ihsw_test_redis_master",
-      upstream: "localhost:6379"
-    };
-    toxiproxy.createProxy(proxyBody)
-      .then((proxy) => {
-        const toxicBody = {
-          attributes: attributes,
-          type: type
-        };
-        return proxy.addToxic(new toxiproxyClient.Toxic(proxy, toxicBody));
-      })
-      .then(resolve)
-      .catch(reject);
-  });
+  const toxiproxy = new toxiproxyClient.Toxiproxy("http://localhost:8474");
+  const proxyBody = {
+    listen: "localhost:0",
+    name: "ihsw_test_redis_master",
+    upstream: "localhost:6379"
+  };
+  return toxiproxy.createProxy(proxyBody)
+    .then((proxy) => {
+      const toxicBody = {
+        attributes: attributes,
+        type: type
+      };
+      return proxy.addToxic(new toxiproxyClient.Toxic(proxy, toxicBody));
+    });
 };
 
 // { attributes: { rate: 1000 },
