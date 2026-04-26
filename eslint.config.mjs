cd522c13d31b7b7013cb4f4304536dc6a94fafee
Adrian Parker-Kilabuk <ihsw.aparker.kilabuk@gmail.com>
2026-04-26 13:07:56 -0500

    feat: start upgrading from axios, other linting deps

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+  js.configs.recommended,
+  tseslint.configs.recommended,
+);
